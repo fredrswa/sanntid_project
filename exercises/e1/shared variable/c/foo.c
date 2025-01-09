fbcfc5def204d87cb4456482f926728c30b7bc13
fredrswa <fredrswa@stud.ntnu.no>
2025-01-09 09:11:07 +0100

    e1

--- exercises/e1/shared variable/c/foo.c
+++ exercises/e1/shared variable/c/foo.c
@@ -5,16 +5,23 @@
 #include <stdio.h>
 
 int i = 0;
+pthread_mutex_t lock;
 
 // Note the return type: void*
 void* incrementingThreadFunction(){
-    // TODO: increment i 1_000_000 times
-    return NULL;
+    for (int j = 1; j < 1000000; j++) {
+        pthread_mutex_lock(&lock);
+        i++;
+        pthread_mutex_unlock(&lock);
+    }
 }
 
 void* decrementingThreadFunction(){
-    // TODO: decrement i 1_000_000 times
-    return NULL;
+    for (int j = 1; j < 100000; j++) {
+        pthread_mutex_lock(&lock);
+        i--;
+        pthread_mutex_unlock(&lock);
+    }
 }
 
 
@@ -22,11 +29,24 @@ int main(){
     // TODO: 
     // start the two functions as their own threads using `pthread_create`
     // Hint: search the web! Maybe try "pthread_create example"?
-    
+    pthread_t increment;
+    pthread_t decrement;
+
+    pthread_mutex_init(&lock, NULL);
+
+    pthread_create(&increment, NULL, incrementingThreadFunction,NULL);
+    pthread_create(&decrement, NULL, decrementingThreadFunction,NULL);
+
     // TODO:
     // wait for the two threads to be done before printing the final result
     // Hint: Use `pthread_join`    
+    pthread_join(increment, NULL);
+    pthread_join(decrement, NULL);
+
+    
     
     printf("The magic number is: %d\n", i);
+
+    pthread_mutex_destroy(&lock);
     return 0;
 }
