c3fcc39e4096d00b7b4bbc823fff2813693b0050
rebekalv <rebekk0607@hotmail.com>
2025-01-13 10:20:43 +0100

    started single elevator

---
+++ single_elevator algo/target/debug/deps/driver_rust-6f26e571ad4c5725.d
@@ -0,0 +1,5 @@
+/home/student/Documents/Gruppe12_TTK4145/Sanntid13/single_elevator algo/target/debug/deps/driver_rust-6f26e571ad4c5725: src/main.rs
+
+/home/student/Documents/Gruppe12_TTK4145/Sanntid13/single_elevator algo/target/debug/deps/driver_rust-6f26e571ad4c5725.d: src/main.rs
+
+src/main.rs:
