269cbc235302a8fa10c196b8e41e7a4ca3d4d9eb
fredrswa <fredrswa@stud.ntnu.no>
2025-01-20 11:50:32 +0100

    new branch testing

--- exercises/e3/driver-rust-0.1.0/src/elevio/elev.rs
+++ exercises/e3/driver-rust-0.1.0/src/elevio/elev.rs
@@ -1,122 +1,15 @@
-#![allow(dead_code)]
+const static N_FLOORS | u8 = 4;
+const static N_BUTTONS | u8 = 3;
 
-use std::fmt;
-use std::io::*;
-use std::net::TcpStream;
-use std::sync::*;
-
-#[derive(Clone, Debug)]
-// elevator has one socket so that th tcp connection can be shared between the threads, the threads is the different functions that are being called
-//defines number of floors
-pub struct Elevator {
-    socket: Arc<Mutex<TcpStream>>,
-    pub num_floors: u8,
-}
-//have to define N_BUTTONS, to use it in the request file
-
-pub const EB_Moving: u8 = 1;
-pub const EB_DoorOpen: u8 = 2;
-pub const EB_Idle: u8 = 3;
-
-pub const HALL_UP: u8 = 0;
-pub const HALL_DOWN: u8 = 1;
-pub const CAB: u8 = 2;
-
-pub const DIRN_DOWN: u8 = u8::MAX;
-pub const DIRN_STOP: u8 = 0;
-pub const DIRN_UP: u8 = 1;
-
-impl Elevator {
-    //initializing the elevator
-    pub fn init(addr: &str, num_floors: u8) -> Result<Elevator> {
-        Ok(Self {
-            socket: Arc::new(Mutex::new(TcpStream::connect(addr)?)),
-            num_floors,
-        })
-    }
-    //addr: address of th TCP server
-
-    //setting the motor direction, call this function to set the direction of the motor
-    pub fn motor_direction(&self, dirn: u8) {
-        let buf = [1, dirn, 0, 0];
-        let mut sock = self.socket.lock().unwrap();
-        sock.write(&buf).unwrap();
-    }
-    //uses TCP connection to send the direction to the elevator
-
-    //setting the call button light, call this function to set the light of the call button
-    pub fn call_button_light(&self, floor: u8, call: u8, on: bool) {
-        let buf = [2, call, floor, on as u8];
-        let mut sock = self.socket.lock().unwrap();
-        sock.write(&buf).unwrap();
-    }
-
-    //setting the floor indicator, call this function to set the floor indicator. inside the elevator
-    pub fn floor_indicator(&self, floor: u8) {
-        let buf = [3, floor, 0, 0];
-        let mut sock = self.socket.lock().unwrap();
-        sock.write(&buf).unwrap();
-    }
-
-    //setting the door light, call this function to set the light of the door
-    pub fn door_light(&self, on: bool) {
-        let buf = [4, on as u8, 0, 0];
-        let mut sock = self.socket.lock().unwrap();
-        sock.write(&buf).unwrap();
-    }
-
-    pub fn stop_button_light(&self, on: bool) {
-        let buf = [5, on as u8, 0, 0];
-        let mut sock = self.socket.lock().unwrap();
-        sock.write(&buf).unwrap();
-    }
-    //call_button function, call this function to get the status of the call button. is it pressed or not
-    pub fn call_button(&self, floor: u8, call: u8) -> bool {
-        let mut buf = [6, call, floor, 0];
-        let mut sock = self.socket.lock().unwrap();
-        sock.write(&mut buf).unwrap();
-        sock.read(&mut buf).unwrap();
-        buf[1] != 0
-    }
-
-    //checking the floor of the elevator
-    pub fn floor_sensor(&self) -> Option<u8> {
-        let mut buf = [7, 0, 0, 0];
-        let mut sock = self.socket.lock().unwrap();
-        sock.write(&buf).unwrap();
-        sock.read(&mut buf).unwrap();
-        if buf[1] != 0 {
-            Some(buf[2])
-        } else {
-            None
-        }
-    }
-    //use this to check if the stop button is pressed
-    pub fn stop_button(&self) -> bool {
-        let mut buf = [8, 0, 0, 0];
-        let mut sock = self.socket.lock().unwrap();
-        sock.write(&buf).unwrap();
-        sock.read(&mut buf).unwrap();
-        buf[1] != 0
-    }
-
-    //use this to check if the obstruction is pressed
-
-    pub fn obstruction(&self) -> bool {
-        let mut buf = [9, 0, 0, 0];
-        let mut sock = self.socket.lock().unwrap();
-        sock.write(&buf).unwrap();
-        sock.read(&mut buf).unwrap();
-        buf[1] != 0
-    }
-
+pub enum DIRN {
+    DIRN_UP = 1,
+    DIRN_STOP = 0,
+    DIRN_DOWN = -1,
 }
 
-//implementing the Display trait for the Elevator struct
-//can formate the output of the Elevator struct so that it is easier to read
-impl fmt::Display for Elevator {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let addr = self.socket.lock().unwrap().peer_addr().unwrap();
-        write!(f, "Elevator@{}({})", addr, self.num_floors)
-    }
+pub enum BUTTON{
+    B_HallUp,
+    B_HallDown,
+    B_Cab
 }
+
--- exercises/e3/driver-rust-0.1.0/src/main.rs
+++ exercises/e3/driver-rust-0.1.0/src/main.rs
@@ -1,81 +1,58 @@
-use std::thread::*;
-use std::time::*;
+use std::sync::{Arc, Mutex};
 
-use crossbeam_channel as cbc;
+const N_FLOORS: u8 = 4;
+const N_BUTTONS: u8 = 3;
 
-use driver_rust::elevio;
-use driver_rust::elevio::elev as e;
+#[derive(Clone, Debug, Copy, PartialEq)]
+enum DIRN {
+    DOWN = -1,
+    STOP = 0,
+    UP = 1,
+}
+#[derive(Clone, Debug, Copy, PartialEq)]
+enum BUTTON {
+    HALL_UP = 0,
+    HALL_DOWN = 1,
+    CAB = 2,
+}
+#[derive(Clone, Debug, Copy, PartialEq)]
+enum ELEV_BEHEVIOUR {
+    IDLE,
+    DOOR_OPEN,
+    MOVING,
+}
+#[derive(Clone, Debug, Copy, PartialEq)]
+enum CLEAR_REQ_VARIANT {
+    CRV_ALL,
+    CRV_IN_DIRN,
+}
+#[derive(Debug)]
+struct ELEVATOR {
+    floor: i32m
+    dirn: DIRN,
+    behaviour: ELEV_BEHEVIOUR::EB_IDLE,
+    requests [[bool; N_BUTTONS]; N_FLOORS],
+    behaviour: ELEV_BEHEVIOUR,
+    config: ELEV_CONFIG,
+}
+#[derive(Clone, Debug)]
+struct ELEV_CONFIG {
+    clear_request_variant: CLEAR_REQ_VARIANT,
+    door_open_duration_sec: f64,
+}
 
-fn main() -> std::io::Result<()> {
-    let elev_num_floors = 4; // number of floors in elevator
-    let elevator = e::Elevator::init("localhost:15657", elev_num_floors)?; // initialize elevator
-    println!("Elevator started:\n{:#?}", elevator);
-
-    let poll_period = Duration::from_millis(25); // polling period, time between each poll
-
-    let (call_button_tx, call_button_rx) = cbc::unbounded::<elevio::poll::CallButton>(); // channel for call button
-    {
-        let elevator = elevator.clone();
-        spawn(move || elevio::poll::call_buttons(elevator, call_button_tx, poll_period)); //spawn is being used to create a new thread
-    } //this is the call button thread
-
-    let (floor_sensor_tx, floor_sensor_rx) = cbc::unbounded::<u8>();
-    {
-        let elevator = elevator.clone();
-        spawn(move || elevio::poll::floor_sensor(elevator, floor_sensor_tx, poll_period));
-    }
-
-    let (stop_button_tx, stop_button_rx) = cbc::unbounded::<bool>();
-    {
-        let elevator = elevator.clone();
-        spawn(move || elevio::poll::stop_button(elevator, stop_button_tx, poll_period));
-    }
-
-    let (obstruction_tx, obstruction_rx) = cbc::unbounded::<bool>();
-    {
-        let elevator = elevator.clone();
-        spawn(move || elevio::poll::obstruction(elevator, obstruction_tx, poll_period));
-    }
-    
-    let mut dirn = e::DIRN_DOWN; 
-    if elevator.floor_sensor().is_none() { // if the floor sensor is not working, the elevator will go down
-        elevator.motor_direction(dirn); // the elevator will go down
-    }
-
-    loop { // loop that listens to the different channels
-        cbc::select! { // select is used to listen to multiple channels at the same time
-            recv(call_button_rx) -> a => { //a is a constant that is used to store the value that is received from the channel
-                let call_button = a.unwrap(); //unwrap is used to get the value from the constant a
-                println!("{:#?}", call_button);
-                elevator.call_button_light(call_button.floor, call_button.call, true); //turns on the light of the call button that is pressed
-            },
-            recv(floor_sensor_rx) -> a => {
-                let floor = a.unwrap();
-                println!("Floor: {:#?}", floor);
-                dirn =
-                    if floor == 0 {
-                        e::DIRN_UP
-                    } else if floor == elev_num_floors-1 {
-                        e::DIRN_DOWN
-                    } else {
-                        dirn
-                    };
-                elevator.motor_direction(dirn);
-            },
-            recv(stop_button_rx) -> a => {
-                let stop = a.unwrap();
-                println!("Stop button: {:#?}", stop);
-                for f in 0..elev_num_floors {
-                    for c in 0..3 {
-                        elevator.call_button_light(f, c, false);
-                    }
-                }
-            },
-            recv(obstruction_rx) -> a => {
-                let obstr = a.unwrap();
-                println!("Obstruction: {:#?}", obstr);
-                elevator.motor_direction(if obstr { e::DIRN_STOP } else { dirn });
+impl Elevator {
+    fn new() -> self {
+        Elevator {
+            floor: -1,
+            dirn: DIRN::STOP,
+            behaviour: ELEV_BEHEVIOUR::IDLE,
+            requests: [[false; N_BUTTONS]; N_FLOORS],
+            config: ELEV_CONFIG {
+                clear_request_variant: CLEAR_REQ_VARIANT::CRV_ALL,
+                door_open_duration_sec: 3.0,
             },
         }
     }
 }
+
