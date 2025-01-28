d2065d0ae52006adcb85265e245dff2a834db77e
fredrswa <fredrswa@stud.ntnu.no>
2025-01-28 13:22:06 +0100

    storage

--- exercises/e3/driver-rust-0.1.0/src/main.rs
+++ exercises/e3/driver-rust-0.1.0/src/main.rs
@@ -54,5 +54,94 @@ impl Elevator {
             },
         }
     }
+    fn print_state(&self) {
+        println!("+-----------------+");
+        println!("|  floor     = {:<2} |", self.floor);
+        println!("|  dirn      = {:<2} |", self.dirn);
+        println!("|  behaviour = {:<2} |", self.behaviour);
+        println!("+-----------------+");
+        println!("|   | UP  | DOWN | CAB  |");
+        for f in (0..N_FLOORS).rev() {
+            print!("| {}", f);
+            for btn in 0..N_BUTTONS {
+                print!("|  {}  ", if self.requests[f][btn] { "#" } else { "-" });
+            }
+            println!("|");
+        }
+        println!("+--------------------+");
+    }
 }
 
+struct ElevatorSystem;
+
+impl ElevatorSystem {
+    pub fn init(addr: &str, num_floors: u8) -> Result<Elevator> {
+        Ok(Self {
+            socket: Arc::new(Mutex::new(TcpStream::connect(addr)?)),
+            num_floors,
+        })
+    }
+    pub fn motor_direction(&self, dirn: u8) {
+        let buf = [1, dirn, 0, 0];
+        let mut sock = self.socket.lock().unwrap();
+        sock.write(&buf).unwrap();
+    }
+    pub fn call_button_light(&self, floor: u8, call: u8, on: bool) {
+        let buf = [2, call, floor, on as u8];
+        let mut sock = self.socket.lock().unwrap();
+        sock.write(&buf).unwrap();
+    }
+    pub fn floor_indicator(&self, floor: u8) {
+        let buf = [3, floor, 0, 0];
+        let mut sock = self.socket.lock().unwrap();
+        sock.write(&buf).unwrap();
+    }
+    pub fn door_light(&self, on: bool) {
+        let buf = [4, on as u8, 0, 0];
+        let mut sock = self.socket.lock().unwrap();
+        sock.write(&buf).unwrap();
+    }
+    pub fn stop_button_light(&self, on: bool) {
+        let buf = [5, on as u8, 0, 0];
+        let mut sock = self.socket.lock().unwrap();
+        sock.write(&buf).unwrap();
+    }
+    pub fn call_button(&self, floor: u8, call: u8) -> bool {
+        let mut buf = [6, call, floor, 0];
+        let mut sock = self.socket.lock().unwrap();
+        sock.write(&mut buf).unwrap();
+        sock.read(&mut buf).unwrap();
+        buf[1] != 0
+    }
+    pub fn floor_sensor(&self) -> Option<u8> {
+        let mut buf = [7, 0, 0, 0];
+        let mut sock = self.socket.lock().unwrap();
+        sock.write(&mut buf).unwrap();
+        sock.read(&mut buf).unwrap();
+        if buf[1] == 0 {
+            None
+        } else {
+            Some(buf[1])
+        }
+    }
+    pub fn stop_button(&self) -> bool {
+        let mut buf = [8, 0, 0, 0];
+        let mut sock = self.socket.lock().unwrap();
+        sock.write(&mut buf).unwrap();
+        sock.read(&mut buf).unwrap();
+        buf[1] != 0
+    }
+    pub fn obstruction(&self) -> bool {
+        let mut buf = [9, 0, 0, 0];
+        let mut sock = self.socket.lock().unwrap();
+        sock.write(&mut buf).unwrap();
+        sock.read(&mut buf).unwrap();
+        buf[1] != 0
+    }
+}
+
+impl fmt::Display for Elevator {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let addr = self.socket.lock().unwrap().peer_addr().unwrap();
+    }
+}
