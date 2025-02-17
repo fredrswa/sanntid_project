5e69922a0633aba3ef2f790a15a94c9fa909fb37
f <f>
2025-02-17 12:01:50 +0100

    started on assigner

---
+++ peer/target/debug/deps/peer-10db022f4bb10a71.d
@@ -0,0 +1,12 @@
+/home/student/Documents/Gruppe12_TTK4145/Sanntid13/peer/target/debug/deps/libpeer-10db022f4bb10a71.rmeta: src/lib.rs src/mod_fsm/config.rs src/mod_fsm/fsm.rs src/mod_fsm/requests.rs src/mod_fsm/timer.rs src/mod_network/network.rs
+
+/home/student/Documents/Gruppe12_TTK4145/Sanntid13/peer/target/debug/deps/libpeer-10db022f4bb10a71.rlib: src/lib.rs src/mod_fsm/config.rs src/mod_fsm/fsm.rs src/mod_fsm/requests.rs src/mod_fsm/timer.rs src/mod_network/network.rs
+
+/home/student/Documents/Gruppe12_TTK4145/Sanntid13/peer/target/debug/deps/peer-10db022f4bb10a71.d: src/lib.rs src/mod_fsm/config.rs src/mod_fsm/fsm.rs src/mod_fsm/requests.rs src/mod_fsm/timer.rs src/mod_network/network.rs
+
+src/lib.rs:
+src/mod_fsm/config.rs:
+src/mod_fsm/fsm.rs:
+src/mod_fsm/requests.rs:
+src/mod_fsm/timer.rs:
+src/mod_network/network.rs:
---
+++ peer/target/debug/deps/peer-e3e77c6b279eb331.d
@@ -0,0 +1,5 @@
+/home/student/Documents/Gruppe12_TTK4145/Sanntid13/peer/target/debug/deps/peer-e3e77c6b279eb331: src/main.rs
+
+/home/student/Documents/Gruppe12_TTK4145/Sanntid13/peer/target/debug/deps/peer-e3e77c6b279eb331.d: src/main.rs
+
+src/main.rs:
