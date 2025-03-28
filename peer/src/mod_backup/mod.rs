f416b7b0ca4598329e874a23865989352b6a8af0
fredrswa <fredrswa@stud.ntnu.no>
2025-03-28 09:41:23 +0100

    documented backup

--- peer/src/mod_backup/mod.rs
+++ peer/src/mod_backup/mod.rs
@@ -1,6 +1,7 @@
-//! Backup module for single peer
-// Runs as a secondary process, listening to the primary process.
-// If the primary process dies, the secondary process takes over.
+//! # Backup Module
+//! Handles startup states of the system
+//! State : Secondary
+//! State : Humble
 
 
 /// Standard Library
@@ -17,18 +18,18 @@ use serde_json;
 use crossbeam_channel::Receiver;
 
 
-/// Internal Modules
+/// Internal Crates
 use crate::mod_hardware;
-use crate::mod_fsm;
 use crate::config::*;
 
-/// CO
+/// Configurations
 static SLEEP_MILLI: u64 = CONFIG.backup.sleep_dur_milli as u64;
 static BACKUP_ADDR: &str = CONFIG.backup.sec_recv;
 static PRIMARY_ADDR: &str = CONFIG.backup.pri_send;
 static MAX_ATTEMPTS: i64 = CONFIG.backup.attempts;
+static UDP_RECV: &str = CONFIG.network.udp_recv;
 
-
+/// Backup State | Listens for latest state from primary, breaks when primary is dead.
 pub fn backup_state () -> (EntireSystem, Option<ElevatorSystem>) {
     
     // Parameters
@@ -75,51 +76,56 @@ pub fn backup_state () -> (EntireSystem, Option<ElevatorSystem>) {
 
 
 
-static UDP_RECV: &str = CONFIG.network.udp_recv;
 
+/// Humble State | Specified startup in an ongoing system
 pub fn humble_state() -> (EntireSystem, Option<ElevatorSystem>) {
-    println!("Starting in humble state: Listening on network for latest state");
-    let socket = UdpSocket::bind(UDP_RECV).expect("Could'nt setup receiver");
+    
     let mut buffer: [u8; 1024] = [0; 1024];
     let mut world_view: EntireSystem = EntireSystem::template();
     let _ = mod_hardware::init();
     let mut es = ElevatorSystem::new();
-    let start_time = std::time::Instant::now();
+    
+    // Socket
+    let socket = UdpSocket::bind(UDP_RECV).expect("Could'nt setup receiver");
 
+    // Listen for 3 seconds
+    let start_time = std::time::Instant::now();
     while start_time.elapsed().as_secs() < 3 {
         socket.set_nonblocking(true).expect("Failed to set non_blocking");
         match socket.recv_from(&mut buffer) {
+            // Valid State
             Ok((amt, _)) => {
                 let received = String::from_utf8_lossy(&buffer[..amt]);
                 if let Ok(parsed) = serde_json::from_str::<EntireSystem>(&received.trim()) {
                     world_view = parsed;
                     drop(socket);
-                    es.update_cab_requests_from_backup();
-                    return (world_view, Some(es));
+                    es.update_cab_requests_from_backup(); // Add cab requests from backup
+                    return (world_view, Some(es)); // Break early if we have valid state.
                 }
             },
-            Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
-                continue;
-            },
-            Err(_) => {
-                continue;
-            }
+            // No state
+            Err(e) if e.kind() == io::ErrorKind::WouldBlock => {continue;},
+            Err(_) => {continue;}
         }
         
     }
+
     drop(socket);
-    es.update_cab_requests_from_backup();
-    return (world_view, Some(es));
+    es.update_cab_requests_from_backup(); // Update Cab requests from backup
+    (world_view, Some(es)) // Single elevator operation
 }
 
-
+// Send Lates Primary | Sends the latest success state to secondary.
 pub fn send_latest_primary(latest_updated_state: Receiver<EntireSystem>) {
+    // Parameters
     let dur = Duration::from_millis(SLEEP_MILLI);
-
     let pri_send = UdpSocket::bind(PRIMARY_ADDR.to_string()).expect("Could'nt setup receiver");
 
+    // Initial State
     let mut ww = EntireSystem::template();
     let mut serialized = serde_json::to_string(&ww).unwrap();
+
+    // Loop
     loop {
         sleep(dur);
         ww = match latest_updated_state.try_recv() {
@@ -128,12 +134,11 @@ pub fn send_latest_primary(latest_updated_state: Receiver<EntireSystem>) {
         };
 
         serialized = serde_json::to_string(&ww).unwrap();
-        pri_send.send_to(serialized.as_bytes(), BACKUP_ADDR);
+        let _ = pri_send.send_to(serialized.as_bytes(), BACKUP_ADDR);
     }
-
 }
 
-
+// Spawn Secondary | Spawns a backup process in a new terminal.
 pub fn spawn_secondary_exe() {
     let path = "./peer";
     let _secondary = Command::new("setsid")
@@ -144,5 +149,5 @@ pub fn spawn_secondary_exe() {
         .stdout(Stdio::null())  // Avoid blocking by suppressing stdout
         .stderr(Stdio::null())  // Suppress stderr
         .spawn()
-        .expect("Failed to start secondary process in new xterm terminal. Start it yourself with cargo run false");
+        .expect("Failed to start secondary process in new xterm terminal. Start it yourself with ./peer <id> humble");
 }
